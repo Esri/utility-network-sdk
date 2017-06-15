ed58a138ffc2a0bb26e2942ba6f656d34c194b3e
Rich Ruh <rruh@esri.com>
2017-06-15 15:28:53 -0600

    Updated data modification script to deal with last-minute data model changes

--- LoadReportSample/ConfigureLoadReportData.py
+++ LoadReportSample/ConfigureLoadReportData.py
@@ -19,10 +19,20 @@ electricDistributionDeviceFeatureClass = arcpy.GetParameterAsText(1)
 
 DomainNetwork = "ElectricDistribution"
 ServicePointCategoryName = "ServicePoint"
+LoadField = "SERVICE_LOAD"
+LoadAttribute = "Customer Load"
 
 # Disable the Utility Network topology
 arcpy.AddMessage("Disabling Utility Network Topology")
 arcpy.un.DisableNetworkTopology(utilityNetwork)
+
+# Create a Network Attribue to represent Load
+arcpy.AddMessage("Creating network attribute")
+arcpy.un.AddNetworkAttribute(utilityNetwork, LoadAttribute, "SHORT", False, None, False)
+
+# Assign the network attribute to the SERVICE_LOAD field in the ElectricDistributionDevice table
+arcpy.AddMessage("Assigning network attribute to field")
+arcpy.un.SetNetworkAttribute(utilityNetwork, LoadAttribute, DomainNetwork, electricDistributionDeviceFeatureClass, LoadField )
 
 # Add a ServicePoint category
 arcpy.AddMessage("Adding Utility Network category")
@@ -33,6 +43,7 @@ arcpy.AddMessage("Assigning Utility Network category")
 arcpy.un.SetNetworkCategory(utilityNetwork, DomainNetwork, electricDistributionDeviceFeatureClass, "Service Point", "Primary Meter", ServicePointCategoryName)
 arcpy.un.SetNetworkCategory(utilityNetwork, DomainNetwork, electricDistributionDeviceFeatureClass, "Service Point", "Single Phase Low Voltage Meter", ServicePointCategoryName)
 arcpy.un.SetNetworkCategory(utilityNetwork, DomainNetwork, electricDistributionDeviceFeatureClass, "Service Point", "Three Phase Low Voltage Meter", ServicePointCategoryName)
+
 
 # Re-enable the Utility Network topology
 arcpy.AddMessage("Enabling Utility Network Topology")
