7a461e743ab90f3d8247b97bf4984b3fb7748b2e
Rich Ruh <rruh@esri.com>
2017-06-16 13:28:46 -0600

    Provide helper routine to get a FeatureClass from a FeatureSource

--- Shared/UtilityNetworkUtils.cs
+++ Shared/UtilityNetworkUtils.cs
@@ -64,5 +64,45 @@ namespace UtilityNetworkSamples
       }
       return null;
     }
+
+    /// <summary>
+    /// GetFeatureClassFromFeatureSource
+    /// 
+    /// In the current beta, the FeatureSource class provides a name property that returns the name of the underlying database table.  
+    /// Unfortunately, when using feature services, the utility network tables are exposed using different names.
+    /// While the underlying database table might be named "ElectricDistributionDevice", the feature class name exposed by the service might
+    /// be "L4Electric_Distribution_Device".  It turns out that the alias of the feature class name matches the name of the
+    /// underlying database table.  This utility function provides a workaround for this issue.
+    /// 
+    /// In particular, this routine can be used to fetch a feature from a FeatureElement that is returned from a network trace.
+    /// 
+    /// Future versions of the utility network API will provide a more elegant solution that does not require a map.
+    /// </summary>
+    /// <param name="map"></param>
+    /// <param name="featureSource"></param>
+    /// <returns></returns>
+    public static FeatureClass GetFeatureClassFromFeatureSource(Map map, FeatureSource featureSource)
+    {
+      // Get the table name from the FeatureSource.  This is the name of the underlying database table.
+      string databaseTableName = featureSource.Name;
+
+      // Now walk through each FeatureLayer in the map.  
+      IReadOnlyList<Layer> layerList = map.GetLayersAsFlattenedList();
+      foreach (Layer layer in layerList)
+      {
+        if (layer is FeatureLayer)
+        {
+          // Now check to see if the feature class alias matches the name we are looking for
+          FeatureLayer featureLayer = layer as FeatureLayer;
+          FeatureClass featureClass = featureLayer.GetFeatureClass();
+          FeatureClassDefinition featureClassDefinition = featureClass.GetDefinition();
+          if (featureClassDefinition.GetAliasName() == databaseTableName)
+          {
+            return featureClass;
+          }
+        }
+      }
+      return null;
+    }
   }
 }
